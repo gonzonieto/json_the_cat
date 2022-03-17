cc0ddcb67347e03b91a061680cbef81b0a169007
‘Gonzo <gonzonieto@gmail.com>
2022-03-17 19:49:34 +0000

    receive and parse JSON string from TheCatAPI

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,10 @@
 const request = require('request');
+
+const breedTerm = process.argv[2];
+const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedTerm;
+
+request(URL, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', JSON.parse(body));
+});
